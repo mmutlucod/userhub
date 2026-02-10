083c3982a283957712cac38c1d507aa7b4f029e9
Mustafa Mutlu <mustfa.muutlu@gmail.com>
2026-02-10 15:29:32 +0300

    Initial Commit

---
+++ userhub/userhubApp.swift
@@ -0,0 +1,17 @@
+//
+//  userhubApp.swift
+//  userhub
+//
+//  Created by MUSTAFA MUTLU on 10.02.2026.
+//
+
+import SwiftUI
+
+@main
+struct userhubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
